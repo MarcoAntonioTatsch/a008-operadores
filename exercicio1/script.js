758d4f1be08c585ddcde9fb1eb2511c3c7a10504
Marco Antonio Tatsch <marco.tatsch@hotmail.com>
2022-07-21 18:31:56 -0300

    atividade concluída com sucesso!

---
+++ exercicio1/script.js
@@ -0,0 +1,8 @@
+
+
+numero = prompt("insira um número par")
+console.log(numero)
+numero %=2
+console.log(numero)
+// todos terminam em 0
+// se colocar um valor impar o resto é sempre 1.
---
+++ exercicio2/script.js
@@ -0,0 +1,21 @@
+let numero1 = prompt('Digite um número')
+let numero2 = prompt('Digite outro número')
+
+console.log("o primeiro número é maior que o segundo?")
+let primeiroMaior = numero1>numero2
+console.log(primeiroMaior)
+
+console.log("o primeiro número é  igual ao segundo?")
+let primeiroIgual = numero1===numero2
+console.log(primeiroIgual)
+
+console.log("o primeiro número é  divisivel pelo segundo?")
+let divisao = numero1%numero2
+let resposta = divisao == 0
+console.log(resposta)
+
+console.log("o segundo número é  divisivel pelo primeiro?")
+let divisao1 = numero2%numero1
+let resposta2 = divisao1 == 1
+console.log(resposta2)
+
