430843ea67bf3e43414bb93900e4216466b3b9da
Marco Antonio Tatsch <marco.tatsch@hotmail.com>
2022-07-21 20:47:40 -0300

    finalizado após feedback dos exercicios

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,8 +1,8 @@
 
 
-numero = prompt("insira um número par")
-console.log(numero)
+numero =Number (prompt("insira um número par"))
+console.log("O número digitado foi ", numero)
 numero %=2
-console.log(numero)
+console.log("O resto da divisaão é ", numero)
 // todos terminam em 0
 // se colocar um valor impar o resto é sempre 1.
