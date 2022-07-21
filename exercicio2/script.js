5029938bf13b4b1b19a0b17d0162c0bd0801b742
Marco Antonio Tatsch <marco.tatsch@hotmail.com>
2022-07-21 18:37:32 -0300

    resolvido um bug do sistema

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -16,6 +16,6 @@ console.log(resposta)
 
 console.log("o segundo número é  divisivel pelo primeiro?")
 let divisao1 = numero2%numero1
-let resposta2 = divisao1 == 1
+let resposta2 = divisao1 == 0
 console.log(resposta2)
 
